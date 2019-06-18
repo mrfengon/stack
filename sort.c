ab88f9893db69b95fd0bb9cbf37185ba6dd1c54a
mrfengon <borniclyde@gmail.com>
2019-06-18 17:38:01 +0300

    freaking sheit

--- sort.c
+++ sort.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include "stack.h"
-#include "funcs.h"
 #include "funcs.c"
 
 void stack_concatenate(stack** head_first, stack** head_second) {
     stack* temp = *head_second, *temp_empty;
     stack_init(&temp_empty, temp->info);
-    element_pop(&temp);
-    while(stack_size(temp)) {
+    element_pop(head_second);
+    while(stack_size(*head_second)) {
         element_push(&temp_empty, temp->info);
-        element_pop(&temp);
+        element_pop(head_second);
+        temp = *head_second;
     }
     while(stack_size(temp_empty)) {
         element_push(head_first, temp_empty->info);
